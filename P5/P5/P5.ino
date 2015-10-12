815bdf0cdff67ceec86f2ee265ee895b34cbcd18
Mikkel Christensen <mikkel@mclc.dk>
2015-10-12 10:28:07 +0200

    Project created

---
+++ P5/P5/P5.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		P5.ino
+ Created:	10/12/2015 10:26:19 AM
+ Author:	Mikkel
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
