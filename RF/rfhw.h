d63a4c09e9155592db341204f4d90ff8324589ed
Søren Bjergmark <sahb1239@gmail.com>
2015-11-04 11:21:30 +0100

    Updated hardware module

--- RF/rfhw.h
+++ RF/rfhw.h
@@ -1,5 +1,6 @@
 namespace rf {
 	void hw_init(uint8_t byte_filter);
-	bool hw_send(const char[] buffer, uint8_t len);
+	bool hw_send(const char buffer[], uint8_t len);
 	bool hw_canSend();
+	char* hw_recieve(uint8_t* length);
 }
