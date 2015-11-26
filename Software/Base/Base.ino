bf1da3aa07763efc23e6f76b2b8fca552162b140
Mikkel Christensen <mikkel@mclc.dk>
2015-11-26 15:10:21 +0100

    Merge into 1 VS solution

---
+++ Software/Base/Base.ino
@@ -0,0 +1,37 @@
+#include <rfpr.h>
+#include <rfhw.h>
+#include <rfapp.h>
+#include <Arduino.h>
+
+// Global variables
+int nextVID = 0;
+char data[20];
+uint16_t connectedSatellites[10];
+
+void setup() {
+	rf::hw_init((uint8_t)GROUP); // Initializing the RF module
+	delay(RF_POWER_UP_TIME); // Waiting for the RF module to power up
+}
+
+void loop() {
+	while (millis() < RF_POWER_UP_TIME + WAIT_FOR_CONNECTS_TIME) // Letting satellites register for WAIT_FOR_CONNECTS_TIME ms
+	{
+		rf::packetTypes type = rf::pr_receive(data);
+		if (type == rf::CONNECT_REQUEST)
+		{
+			struct rf::connectRequest *request;
+			request = (rf::connectRequest*)data;
+			uint16_t satelliteRID = (request->RID);
+
+			rf::pr_send_connectedConfirmation(satelliteRID, nextVID++);
+		}
+	}
+
+	for (auto i = 0; i < nextVID; i++)
+	{
+		rf::pr_send_ping((char)i);
+		delay(TIME_BETWEEN_PING / nextVID);
+	}
+
+	// Receive, check if DataSending packet. Do some stuff with that.
+}
