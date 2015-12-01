a5df5ade6ce358758f3e3b07b9667855d6365f40
Mikkel Christensen <mikkel@mclc.dk>
2015-12-01 12:52:46 +0100

    Moved to a function

--- RMPS/Base/Base.ino
+++ RMPS/Base/Base.ino
@@ -12,24 +12,17 @@ int satelliteNumber = 0;
 unsigned long int pingSent = 0;
 uint16_t connectedSatellites[10];
 
+// Prototypes
+void registerSatellite();
+
 void setup() {
 	rf::hw_init((uint8_t)GROUP); // Initializing the RF module
 	delay(RF_POWER_UP_TIME); // Waiting for the RF module to power up
 }
 
 void loop() {
-	while (millis() < RF_POWER_UP_TIME + WAIT_FOR_CONNECTS_TIME) // Letting satellites register for WAIT_FOR_CONNECTS_TIME ms
-	{
-		rf::packetTypes type = rf::pr_receive(data);
-		if (type == rf::CONNECT_REQUEST)
-		{
-			struct rf::connectRequest *request;
-			request = (rf::connectRequest*)data;
-			uint16_t satelliteRID = (request->RID);
-
-			rf::pr_send_connectedConfirmation(satelliteRID, nextVID++);
-		}
-	}
+	while (millis() < RF_POWER_UP_TIME + WAIT_FOR_CONNECTS_TIME) 
+		registerSatellite(); // Letting satellites register for WAIT_FOR_CONNECTS_TIME ms
 
 	rf::pr_send_ping((char)satelliteNumber);
 	pingSent = millis();
@@ -47,10 +40,23 @@ void loop() {
 	for (int i = 0; i < 20; i++) // HARDCODE!!
 	{
 		Serial.print(satelliteNumber);
-		Serial.print(data[i]);
+		Serial.print(data[i]); // TODO Check for out of bounds
 		Serial.print(millis());
 	}
 
 	delay(WAIT_FOR_CONNECTS_TIME - (millis() - pingSent)); 
 	satelliteNumber++;
+}
+
+void registerSatellite()
+{
+	rf::packetTypes type = rf::pr_receive(data);
+	if (type == rf::CONNECT_REQUEST)
+	{
+		struct rf::connectRequest *request;
+		request = (rf::connectRequest*)data;
+		uint16_t satelliteRID = (request->RID);
+
+		rf::pr_send_connectedConfirmation(satelliteRID, nextVID++);
+	}
 }
