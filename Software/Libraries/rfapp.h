e81c2a116490913d53d01012667a8329b723aa84
Mikkel Christensen <mikkel@mclc.dk>
2015-12-04 11:58:14 +0100

    Removed lots of crap - and added uppaal

--- Software/Libraries/rfapp.h
+++
@@ -1,6 +0,0 @@
-// Global constants
-#define GROUP 20
-#define SAMPLE_ARRAY_SIZE 50
-#define TIME_BETWEEN_PING 200 // We will get pinged for data at least every x ms
-#define RF_POWER_UP_TIME 3000
-#define WAIT_FOR_CONNECTS_TIME 5000 // 5 seconds
