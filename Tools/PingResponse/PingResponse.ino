04736d1fff3193d178ac528d0e061da173cbd288
Søren Bjergmark <sahb1239@gmail.com>
2015-12-19 02:05:10 +0100

    Added comments to 2 of the tools

--- Tools/PingResponse/PingResponse.ino
+++ Tools/PingResponse/PingResponse.ino
@@ -1,9 +1,9 @@
 #include <rfhw.h>
 #include "Arduino.h"
 
+// Size of packages - needs to match with sender
 #define SEND_PACKET 85
-#define receive_SIZE 10
-//#define WAIT 1
+#define RECEIVE_PACKET 10
 
 uint8_t arraySend[SEND_PACKET];
 
@@ -11,50 +11,34 @@ void setup() {
   // Init array
   for (int i = 0; i < SEND_PACKET; i++) {
     arraySend[i] = 'a';
-  }  
-
-  // Init serial
-//  Serial.begin(57600);
+  }
 
   // Init RF module
   rf::phy_init((uint8_t)20);
 
   // Give time for bootup
   delay(500);
-  
-//  Serial.println("Init done");
 }
 
 void loop() {
   // receive data
   uint8_t len = 0;
-  volatile uint8_t* rdata = rf::phy_receive(&len);
+  uint8_t* rdata = rf::phy_receive(&len);
   if (rdata != NULL) {
-      if (len == receive_SIZE) {
+      // When data is received (and it has correct length, send data again)
+      if (len == RECEIVE_PACKET) {
         SendData();
       }
   }
 }
 
 void SendData() {
+  // Give receiver time to turn on the receiver on again
   delayMicroseconds(3);
 
-#ifdef WAIT
-  uint32_t start = millis();
-#endif
-
-#ifndef WAIT
+  // Send data
   if (!rf::phy_send(arraySend, SEND_PACKET))
-#else
-  if (!rf::phy_sendWait(arraySend, SEND_PACKET))
-#endif
   {
-//    Serial.println("NOT SEND");
     return;
   }
-
-#ifdef WAIT
-  uint32_t result = millis() - start;
-  Serial.println(result);
-#endif
 }
--- Tools/SendRecieve/SendRecieve.ino
+++ Tools/SendRecieve/SendRecieve.ino
@@ -1,12 +1,14 @@
 #include <rfhw.h>
 #include "Arduino.h"
 
+// Size of packages - needs to match with sender
 #define SEND_PACKET 10
-#define receive_SIZE 85
-//#define WAIT 1
+#define RECEIVE_PACKET 85
+
+// Timeout of packet to be received
+#define RECEIVE_TIMEOUT 200
 
 uint8_t arraySend[SEND_PACKET];
-uint8_t arrayreceive[receive_SIZE];
 uint32_t lastSendTime = 0;
 
 void setup() {
@@ -28,57 +30,47 @@ void setup() {
 void loop() {
   // receive data
   uint8_t len = 0;
-  volatile uint8_t* rdata = rf::phy_receive(&len);
+  uint8_t* rdata = rf::phy_receive(&len);
   if (rdata != NULL) {
-    if (len == receive_SIZE) {
-      uint32_t current = millis();
-      
-      int calculatedTime = current - lastSendTime;
-
-      #ifndef WAIT
+    // When data is received (and it has correct length, send data again)
+    if (len == RECEIVE_PACKET) {
+      // Get time it took to recieve package
+      int calculatedTime = millis() - lastSendTime;
       Serial.println(calculatedTime);
-      #endif
+
+      // Send data - function handles delay
       SendData();
     }
   }
 
-    if (Serial.available() > 0) {
+  // Read from serial to support sending on recieving a s
+  if (Serial.available() > 0) {
+    // read the incoming byte:
+    char incomingByte = Serial.read();
 
-      // read the incoming byte:
-      char incomingByte = Serial.read();
-
-      if (incomingByte == 's')
-        SendData();
-    }
-
-    if (millis() - lastSendTime > 200) {
-      Serial.println("r");
+    // Send data if byte is s
+    if (incomingByte == 's')
       SendData();
-    }
+  }
+
+  // Handle timeout of packet and send a new if not received any answer
+  if (millis() - lastSendTime > RECEIVE_TIMEOUT) {
+    Serial.println("r");
+    SendData();
+  }
 }
 
 void SendData() {
-  lastSendTime = millis();
+  // Wait 5 ms to delay execution of sending next packet
   delay(5);
 
-#ifdef WAIT
-  uint32_t start = millis();
-#endif
+  // Update last time sent
+  lastSendTime = millis();
 
-#ifndef WAIT
+  // Send package
   if (!rf::phy_send(arraySend, SEND_PACKET))
-#else
-  if (!rf::phy_sendWait(arraySend, SEND_PACKET))
-#endif
   {
     Serial.println("NOT SEND");
     return;
   }
-
-#ifdef WAIT
-  uint32_t result = millis() - start;
-  Serial.println(result);
-#endif
-  
-  lastSendTime = millis();
 }
